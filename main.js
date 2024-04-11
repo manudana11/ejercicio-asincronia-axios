7285323f321c7f42f3cf3d276bbde87f8f69c078
manudana11 <manueldanapallardo@gmail.com>
2024-04-11 19:22:48 +0200

    terminado a mi manera

--- main.js
+++ main.js
@@ -33,7 +33,7 @@ console.log(users);
 // Crear una función llamada "showUsers" que muestre por consola la variable global "users" que has creado.
 
 const showUsers = () => {
-    console.log(users);
+    console.log('ejercicio 4', users);
 }
 
 // Crea un botón que cuando lo cliques ejecute la función que habías creado
@@ -66,10 +66,107 @@ btn.addEventListener('click', (e) => {
 // En este ejercicio utilizaremos la API de https://dog.ceo/dog-api/. Leyendo su documentación, deberás hacer lo siguiente:
 
 
+
 // Imprimir por consola la lista de razas de todos los perros.
+
+axios.get('https://dog.ceo/api/breeds/list/all')
+.then((res) => console.log('extra 1', res))
+.catch((err) => console.log(err));
+
 // Imprimir por consola una imagen random de una raza.
+
+axios.get('https://dog.ceo/api/breeds/image/random')
+.then((res) => console.log('extra 2', res.data))
+.catch((err) => console.log(err));
+
 // Imprimir por consola todas las imágenes de una raza concreta.
+
+axios.get('https://dog.ceo/api/breed/hound/afghan/images')
+.then((res) => console.log('extra 3', res.data)).catch((err) => console.log(err));
 
 // Recuerda que para estos ejercicios deberás utilizar Axios. Al haber conseguido que se imprima por consola, el siguiente paso será que se muestren en pantalla con las herramientas que nos ofrece el árbol DOM.
 
+const btn2 = document.getElementById('imag');
+let allImg = [];
+axios.get('https://dog.ceo/api/breed/hound/afghan/images')
+.then((res) => allImg = res.data.message)
+.catch((err) => console.log(err));
+
+console.log(allImg);
+//console.log(btn2);
+btn2.addEventListener('click', (e) => {
+    const imgPerritos = document.getElementById('imagPerr');
+    for (let i = 0; i < allImg.length; i++) {
+        const domImg = document.createElement('div');
+        domImg.innerHTML = `<img src="${allImg[i]}" alt="perritos">`;
+        imgPerritos.appendChild(domImg);
+    }
+})
+
 // *Extra ¿Y si ahora te pidiéramos que podamos poner otra raza en la url para que nos busque otras imágenes? Adapta las urls que ya tenías para que puedas pasarle argumentos.
+//https://images.dog.ceo/breeds/hound-afghan/n02088094_988.jpg
+
+const btn3 = document.getElementById('razaRand');
+const btn4 = document.getElementById('imgRazaRa');
+//console.log(btn4);
+
+let allBreed = [];
+axios.get('https://dog.ceo/api/breeds/list/all').then((res) => allBreed = Object.keys(res.data.message)).catch((err) => console.log(err));
+
+let allImgRand = [];
+
+btn3.addEventListener('click', (e) => {
+    console.log(allBreed);
+    const selectBreed = Math.floor(Math.random() * allBreed.length);
+    const randomBreed = allBreed[selectBreed];
+    console.log(randomBreed);
+    const textoRaza = document.getElementById('textRaza');
+    textoRaza.innerHTML = `La raza aleatoria es: ${randomBreed}`;
+    axios.get(`https://dog.ceo/api/breed/${randomBreed}/images`).then((res) => allImgRand = res.data.message).catch((err) => console.log(err));
+    btn4.addEventListener('click', (e) => {
+        const fotosPerros = document.getElementById('fotosPerros');
+        for (let i = 0; i < allImgRand.length; i++) {
+            const imgRandomDog = document.createElement('div');
+            imgRandomDog.innerHTML = `<img src="${allImgRand[i]}" alt="perritos">`;
+            fotosPerros.appendChild(imgRandomDog);
+        }
+    })
+})
+
+// RECORDATORIO DE QUE HAY QUE PENSAR ANTES DE EDITAR CODIGO
+
+// const btn2 = document.getElementById('imag');
+
+// let allImg = [];
+// axios.get(`https://dog.ceo/api/breed/hound/afghan/images`)
+// .then((res) => allImg = res.data.message)
+// .catch((err) => console.log(err));
+
+// let allBreed = [];
+// axios.get('https://dog.ceo/api/breeds/list/all')
+// .then((res) => {
+//     allBreed = Object.keys(res.data.message);
+//     const selectImg = Math.floor(Math.random() * allBreed.length);
+//     const randomImg = allBreed[selectImg];
+// })
+// .catch((err) => console.log(err));
+
+// axios.get(`https://dog.ceo/api/breed/hound/afghan/images`)
+// .then((res) => allImg = res.data.message)
+// .catch((err) => console.log(err));
+
+
+// console.log(allImg);
+// //console.log(btn2);
+// btn2.addEventListener('click', (e) => {
+//     const imgPerritos = document.getElementById('imagPerr');
+//     for (let i = 0; i < allBreed.length; i++) {
+//         const domImg = document.createElement('div');
+
+//         domImg.innerHTML = `
+//             <img src="${allImg[i]}" alt="perritos">`;
+//         imgPerritos.appendChild(domImg);
+//     }
+//     console.log('razas', allBreed);
+//     console.log('imagenes', allImg)
+// })
